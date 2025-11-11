08dea0ee4b95c3c1002a7a263fb7b7d4476680fe
alexandrakomkova <sashakomkova2001@gmail.com>
2025-11-11 05:36:25 -0800

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_ce/hive.dart';
 import 'package:hive_ce_flutter/adapters.dart';
-import 'package:todo_app/presentation/app.dart';
+import 'package:todo_app/app/app.dart';
 
+import 'app/app_bloc_observer.dart';
 import 'data/data_source/hive_todo_adapter.dart';
 import 'data/repository/todo_repository_impl.dart';
 import 'di/injection.dart';
@@ -16,6 +18,8 @@ Future<void> main() async {
 
   final box = await Hive.openBox<Todo>('todos');
   print('box length: ${box.length}');
+
+  Bloc.observer = const AppBlocObserver();
 
   runApp(
       App(createTodoRepositoryImpl: () => TodoRepositoryImpl(box))
