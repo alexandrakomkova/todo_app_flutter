e955817605fb29803e142e05edbe2005769b31e0
alexandrakomkova <sashakomkova2001@gmail.com>
2025-11-13 23:09:24 -0800

    remove unused di

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_ce/hive.dart';
 import 'package:hive_ce_flutter/adapters.dart';
 import 'package:todo_app/app/app.dart';
 
 import 'app/app_bloc_observer.dart';
 import 'data/data_source/hive_todo_adapter.dart';
 import 'data/repository/todo_repository_impl.dart';
-import 'di/injection.dart';
 import 'domain/model/todo.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
-  configureInjection();
 
   final box = await Hive.openBox<Todo>('todos');
   //print('box length: ${box.length}');
--- lib/presentation/theme/theme.dart
+++ lib/presentation/theme/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/presentation/widget/todo_card.dart';
 
 class TodoTheme {
 
